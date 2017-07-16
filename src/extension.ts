ab761296030354edab850d74ebc51c7e54797b8c
Sanders Lauture <golf1052@msn.com>
2017-07-16 12:16:21 -0700

    Remove unnecessary files and comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,10 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 var cjson = require('strip-json-comments');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('base16.generator.activateTheme', function () {
         activateTheme();
     });
@@ -71,6 +63,5 @@ function parseJson(text: string) {
     return JSON.parse(cjson(text));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
